c7fc9c2fc75445ac9f02879bc58390ca34e294f7
RikuLaaksonen <riku.laaksonen@student.hamk.fi>
2021-11-24 17:55:22 +0200

    Kohdat 1-3

--- src/AutoHarjoitus.java
+++ src/AutoHarjoitus.java
@@ -2,8 +2,48 @@
 public class AutoHarjoitus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
+
+}
+
+class Auto {
+	
+	public String merkki;
+	public String malli;
+	public int bensanMaara;
+	
+	
+	public void Jarruta()
+
+	{
+		System.out.println("Auto Jarruttaa");
+	}
+	
+	public void Kiihdyta ()
+	{
+		System.out.println("Auto kiihtyy");
+		bensanMaara = bensanMaara -1;
+	}
+	
+	public void NaytaTiedot()
+	{
+		System.out.println(merkki + malli + bensanMaara);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
