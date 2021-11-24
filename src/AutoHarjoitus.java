b08e7bbb390938dd9bf59f46348eabced4ab32d7
RikuLaaksonen <riku.laaksonen@student.hamk.fi>
2021-11-24 18:28:17 +0200

    kohdat 4-6

--- src/AutoHarjoitus.java
+++ src/AutoHarjoitus.java
@@ -3,6 +3,15 @@ public class AutoHarjoitus {
 
 	public static void main(String[] args) {
 
+		Auto auto1 = new Auto();
+		auto1.merkki="Audi";
+		auto1.malli="S3";
+		auto1.bensanMaara= 20;
+		auto1.naytaTiedot();
+		auto1.kiihdyta();
+		
+		
+		
 
 	}
 
@@ -15,24 +24,36 @@ class Auto {
 	public int bensanMaara;
 	
 	
-	public void Jarruta()
+	public void jarruta()
 
 	{
 		System.out.println("Auto Jarruttaa");
 	}
 	
-	public void Kiihdyta ()
-	{
-		System.out.println("Auto kiihtyy");
+	public void kiihdyta ()
+	{if (bensanMaara>0)
+	
+	{	System.out.println("Auto kiihtyy");
 		bensanMaara = bensanMaara -1;
 	}
-	
-	public void NaytaTiedot()
-	{
-		System.out.println(merkki + malli + bensanMaara);
 	}
 	
+	public void naytaTiedot()
+	{
+		System.out.println("Merkki: " +merkki + " Malli: " + malli + " Bensan määrä: " + bensanMaara);
+	}
 	
+	public void tankkaa(int maara)
+	{
+		if (maara>0)
+		{
+			System.out.println("Tankissa bensaa: "+ bensanMaara);
+			System.out.println("Tankkaus: " + maara);
+			System.out.println("Tankissa bensaa tankkauksen jälkeen: " + maara + bensanMaara);
+		}
+		
+		
+	}
 	
 	
 	
